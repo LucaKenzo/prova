23baacdd18df5832b09bff0bcec3cc9c796c1c8b
LucaKenzo <luca.kenzo0204@gmail.com>
2024-04-11 12:30:13 -0300

    deucerto

---
+++ DiceRoller/DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DiceRoller_Clicked(object sender, EventArgs e)
+        {
+            int selecionarLados = Convert.ToInt32(picker.SelectedItem);
+            int sorteio = new Random().Next(1, selecionarLados +1);
+            Dado.Text = sorteio.ToString(); 
+        }
+    }
+
+}
